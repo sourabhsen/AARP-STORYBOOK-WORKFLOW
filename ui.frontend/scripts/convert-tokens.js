ed7ed04a56704101db6769067c86661b5aedc876
sourabhsen05 <ssen@aarp.org>
2024-07-11 12:39:34 -0400

    changes

---
+++ ui.frontend/scripts/convert-tokens.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const tokens = require('./tokens.json');
+
+let scssContent = '';
+
+for (const [key, value] of Object.entries(tokens)) {
+    scssContent += `$${key}: ${value};\n`;
+}
+
+fs.writeFileSync('tokens.scss', scssContent);
---
+++ ui.frontend/stories/pageAuthoring/button.stories.js
@@ -0,0 +1,39 @@
+
+import buttonPolicy from '../../storybook-assets/policies/button.json';
+
+
+export default {
+  title: 'pageAuthoring/Button',
+  tags: ['autodocs'],
+  decorators: [],
+  parameters: { 
+    myAddonParameter: {
+      policy: buttonPolicy 
+    } 
+  }
+};
+
+window.addEventListener('message', (detail) => {
+  const detailsObj = detail?.data || {};
+  const eventType = JSON.parse(detailsObj).event?.type;
+  
+  if (eventType === 'storybook/style-system/change') {
+    const val = JSON.parse(detailsObj).event;
+    const styleClasses = val.args[0].selectedStyle;
+    const getHtml = document.querySelector('#storybook-root');
+    const isWrapper = document.querySelector('#storybook-root .sb-wrapper');
+    
+    const updatedHTML = `<div class="sb-wrapper ${styleClasses}">${getHtml.innerHTML}</div>`;
+    
+    if (isWrapper) {
+      document.querySelector('#storybook-root .sb-wrapper').classList.forEach(className => {
+        if (className !== 'sb-wrapper') {
+          document.querySelector('#storybook-root .sb-wrapper').classList.remove(className);
+        }
+      });
+      document.querySelector('#storybook-root .sb-wrapper').classList.add(styleClasses);
+    } else {
+      getHtml.innerHTML = updatedHTML;
+    }
+  }
+});

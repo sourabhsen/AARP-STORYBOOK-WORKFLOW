c6dea12348d599f710b3c54e4d610c11643e63c8
sourabhsen05 <ssen@aarp.org>
2024-07-18 14:38:08 -0400

    Test workflow from token

--- ui.frontend/.storybook/preview.ts
+++ ui.frontend/.storybook/preview.ts
@@ -1,4 +1,3 @@
 import '../storybook-assets/resources/clientlib-base.min.css';
 import '../storybook-assets/resources/clientlib-base.min.js';
-import '../../ui.frontend/tokens.scss';
-
+import '../scripts/design-tokens.css';
